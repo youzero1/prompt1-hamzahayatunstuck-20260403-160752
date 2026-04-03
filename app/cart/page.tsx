59932ac64aa8d7b14500f60ac835b529081f8e95
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-04-03 16:12:51 +0500

    Initial commit from Flare Codegen (API)

---
+++ app/cart/page.tsx
@@ -0,0 +1,10 @@
+import CartPage from '@/components/CartPage';
+
+export const metadata = {
+  title: 'Shopping Cart - ShopNext',
+  description: 'Review your shopping cart'
+};
+
+export default function Cart() {
+  return <CartPage />;
+}
