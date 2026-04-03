3ebc526492182473999cc93d1dab06255444471a
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-04-03 16:12:52 +0500

    Initial commit from Flare Codegen (API)

---
+++ app/checkout/page.tsx
@@ -0,0 +1,10 @@
+import CheckoutForm from '@/components/CheckoutForm';
+
+export const metadata = {
+  title: 'Checkout - ShopNext',
+  description: 'Complete your purchase'
+};
+
+export default function CheckoutPage() {
+  return <CheckoutForm />;
+}
